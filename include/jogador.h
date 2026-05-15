9f5717ff717372533e9103b9fb82b9f87b0bf99b
yan791 <yrn@cesar.school>
2026-05-15 08:49:36 -0300

    jogador.h finalizado e prototipos de funcoes

--- include/jogador.h
+++ include/jogador.h
@@ -4,3 +4,22 @@
 #include "raylib.h"
 
 #define TOTAL_PISTAS 3
+
+typedef struct Jogador {
+    int pista;
+    float x;
+    float baseY;
+    float y;
+    float largura;
+    float altura;
+    float velocidadeY;
+    bool pulando;
+} Jogador;
+
+void inicializarJogador(Jogador *jogador);
+void atualizarJogador(Jogador *jogador, float delta);
+void desenharJogador(Jogador jogador, Texture2D sprite);
+Rectangle obterRetanguloJogador(Jogador jogador);
+float obterYDaPista(int pista);
+
+#endif
