d6275f42feb55caf088947a1382062a0455a214e
yan791 <yrn@cesar.school>
2026-05-16 10:46:17 -0300

    add atualizacao do jogador

--- src/jogador.c
+++ src/jogador.c
@@ -18,3 +18,34 @@ void inicializarJogador(Jogador *jogador) {
     jogador->velocidadeY = 0.0f;
     jogador->pulando = false;
 }
+
+void atualizarJogador(Jogador *jogador, float delta) {
+    if ((IsKeyPressed(KEY_A) || IsKeyPressed(KEY_LEFT)) && jogador->pista > 0) {
+        jogador->pista--;
+    }
+
+    if ((IsKeyPressed(KEY_D) || IsKeyPressed(KEY_RIGHT)) && jogador->pista < TOTAL_PISTAS - 1) {
+        jogador->pista++;
+    }
+
+    jogador->baseY = obterYDaPista(jogador->pista);
+
+    if (IsKeyPressed(KEY_SPACE) && !jogador->pulando) {
+        jogador->pulando = true;
+        jogador->velocidadeY = -690.0f;
+    }
+
+    if (jogador->pulando) {
+        jogador->velocidadeY += 1850.0f * delta;
+        jogador->y += jogador->velocidadeY * delta;
+
+        float chao = jogador->baseY - jogador->altura;
+        if (jogador->y >= chao) {
+            jogador->y = chao;
+            jogador->velocidadeY = 0.0f;
+            jogador->pulando = false;
+        }
+    } else {
+        jogador->y = jogador->baseY - jogador->altura;
+    }
+}
